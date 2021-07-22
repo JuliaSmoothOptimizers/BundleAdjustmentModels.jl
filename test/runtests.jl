7f047d9f233a3abe7cd9a5e823630db02c438ba2
AntoninKns <antonin.kenens@etu.enseeiht.fr>
2021-07-22 10:24:10 -0400

    Correctif tests 1.3 -> 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
-using BALNLSModels, Test, NLPModels, DataFrames
+using BALNLSModels, Test, NLPModels, DataFrames, Pkg
+
+Pkg.PlatformEngines.probe_platform_engines!()
 
 include("../src/BALProblemsList.jl")
 include("testBALNLSFunctions.jl")
