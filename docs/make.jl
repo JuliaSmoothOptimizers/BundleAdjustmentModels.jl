e085a4c6160dfb81998b98e7ecf62702b72efa33
AntoninKns <antonin.kenens@etu.enseeiht.fr>
2021-07-26 12:20:46 -0400

    Ajout doc 2e essai

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using Documenter
+using Printf
+using BALNLSModels
+
+# Add index.md file as introduction to navigation menu
+pages = [
+  "Introduction" => "index.md",
+]
+
+makedocs(
+  sitename = "BALNLSModels",
+  format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+  modules = [BALNLSModels],
+  pages = pages,
+)
+
+deploydocs(
+  repo = "github.com/JuliaSmoothOptimizers/BALNLSModels",
+  push_preview = true,
+  devbranch = "main",
+)
