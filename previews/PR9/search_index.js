820cca66f171bbe76bda94805ada3479c3f6ed73
Documenter.jl <documenter@juliadocs.github.io>
2021-07-26 17:59:09 +0000

    build based on 19cd56a

---
+++ previews/PR9/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Home","page":"Introduction","title":"BALNLSModels documentation","text":"","category":"section"},{"location":"","page":"Introduction","title":"Introduction","text":"Julia repository of bundle adjustment problems from the Bundle Adjustment in the Large repository.","category":"page"}]
+}
