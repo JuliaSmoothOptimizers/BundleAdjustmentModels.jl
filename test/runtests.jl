0a08cbb3bf5424864751e5daa248da5e6440ddfe
AntoninKns <49387118+AntoninKns@users.noreply.github.com>
2021-07-29 11:20:31 -0400

    Ajout tests résidu et jacobien (#18)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using BALNLSModels, Test, NLPModels, DataFrames, Pkg
+using BALNLSModels, DataFrames, LinearAlgebra, NLPModels, Pkg, Test
 
 if VERSION ≤ VersionNumber(1, 6, 0)
   Pkg.PlatformEngines.probe_platform_engines!()
