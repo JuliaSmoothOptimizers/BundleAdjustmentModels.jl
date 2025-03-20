3f5bfea7cd3da61f2925d04eaefb415b20a1b54f
amontoison <amontoison@users.noreply.github.com>
2025-03-20 04:52:25 +0000

    :robot: Format .jl files

--- test/testBundleAdjustmentAllocations.jl
+++ test/testBundleAdjustmentAllocations.jl
@@ -6,28 +6,27 @@ meta_nls = nls_meta(model)
 S = typeof(model.meta.x0)
 
 @testset "residual allocations" begin
-    F = S(undef, meta_nls.nequ)
+  F = S(undef, meta_nls.nequ)
 
-    residual!(model, model.meta.x0, F)
-    residual_alloc(model) = @allocated residual!(model, model.meta.x0, F)
-    @test residual_alloc(model) == 0
+  residual!(model, model.meta.x0, F)
+  residual_alloc(model) = @allocated residual!(model, model.meta.x0, F)
+  @test residual_alloc(model) == 0
 end
 
 @testset "jac_structure_residual allocations" begin
-    rows = Vector{Int}(undef, meta_nls.nnzj)
-    cols = Vector{Int}(undef, meta_nls.nnzj)
+  rows = Vector{Int}(undef, meta_nls.nnzj)
+  cols = Vector{Int}(undef, meta_nls.nnzj)
 
-    jac_structure_residual!(model, rows, cols)
-    jac_structure_residual_alloc(model, rows, cols) =
-        @allocated jac_structure_residual!(model, rows, cols)
-    @test jac_structure_residual_alloc(model, rows, cols) == 0
+  jac_structure_residual!(model, rows, cols)
+  jac_structure_residual_alloc(model, rows, cols) =
+    @allocated jac_structure_residual!(model, rows, cols)
+  @test jac_structure_residual_alloc(model, rows, cols) == 0
 end
 
 @testset "jac_coord_residual allocations" begin
-    vals = S(undef, meta_nls.nnzj)
+  vals = S(undef, meta_nls.nnzj)
 
-    jac_coord_residual!(model, model.meta.x0, vals)
-    jac_coord_residual_alloc(model, vals) =
-        @allocated jac_coord_residual!(model, model.meta.x0, vals)
-    @test jac_coord_residual_alloc(model, vals) == 0
+  jac_coord_residual!(model, model.meta.x0, vals)
+  jac_coord_residual_alloc(model, vals) = @allocated jac_coord_residual!(model, model.meta.x0, vals)
+  @test jac_coord_residual_alloc(model, vals) == 0
 end
