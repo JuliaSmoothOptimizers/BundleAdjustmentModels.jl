694112bc1b7588ea8d02de4cc0b08d6c3944625b
amontoison <amontoison@users.noreply.github.com>
2025-03-20 04:52:25 +0000

    :robot: Format .jl files

--- src/ReadFiles.jl
+++ src/ReadFiles.jl
@@ -18,41 +18,41 @@ Expected file structure:
   - 3D points: 3 lines per point
 """
 function readfile(filename::String; T::Type = Float64)
-    open(filename) do io
-        f = Bzip2DecompressorStream(io)
+  open(filename) do io
+    f = Bzip2DecompressorStream(io)
 
-        # First line: number of cameras, points, and observations
-        ncams, npnts, nobs = map(x -> parse(Int, x), split(readline(f)))
-        @debug "$filename: reading $ncams cameras, $npnts points, $nobs observations"
+    # First line: number of cameras, points, and observations
+    ncams, npnts, nobs = map(x -> parse(Int, x), split(readline(f)))
+    @debug "$filename: reading $ncams cameras, $npnts points, $nobs observations"
 
-        # Preallocate vectors
-        cam_indices = Vector{Int}(undef, nobs)
-        pnt_indices = Vector{Int}(undef, nobs)
-        pt2d = Vector{T}(undef, 2 * nobs)
-        x0 = Vector{T}(undef, 3 * npnts + 9 * ncams)
+    # Preallocate vectors
+    cam_indices = Vector{Int}(undef, nobs)
+    pnt_indices = Vector{Int}(undef, nobs)
+    pt2d = Vector{T}(undef, 2 * nobs)
+    x0 = Vector{T}(undef, 3 * npnts + 9 * ncams)
 
-        # Read observations
-        for i = 1:nobs
-            cam, pnt, x, y = split(readline(f))
-            cam_indices[i] = parse(Int, cam) + 1  # Convert to 1-based indexing
-            pnt_indices[i] = parse(Int, pnt) + 1
-            pt2d[2 * i - 1] = parse(T, x)
-            pt2d[2 * i] = parse(T, y)
-        end
-
-        # Read camera parameters (9 values per camera)
-        for i = 1:ncams
-            offset = 3 * npnts + 9 * (i - 1)
-            for j = 1:9
-                x0[offset + j] = parse(T, readline(f))
-            end
-        end
-
-        # Read 3D points (3 coordinates per point)
-        for k = 1:(3 * npnts)
-            x0[k] = parse(T, readline(f))
-        end
-
-        return cam_indices, pnt_indices, pt2d, x0, ncams, npnts, nobs
+    # Read observations
+    for i = 1:nobs
+      cam, pnt, x, y = split(readline(f))
+      cam_indices[i] = parse(Int, cam) + 1  # Convert to 1-based indexing
+      pnt_indices[i] = parse(Int, pnt) + 1
+      pt2d[2 * i - 1] = parse(T, x)
+      pt2d[2 * i] = parse(T, y)
     end
+
+    # Read camera parameters (9 values per camera)
+    for i = 1:ncams
+      offset = 3 * npnts + 9 * (i - 1)
+      for j = 1:9
+        x0[offset + j] = parse(T, readline(f))
+      end
+    end
+
+    # Read 3D points (3 coordinates per point)
+    for k = 1:(3 * npnts)
+      x0[k] = parse(T, readline(f))
+    end
+
+    return cam_indices, pnt_indices, pt2d, x0, ncams, npnts, nobs
+  end
 end
