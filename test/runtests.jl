9c07a6e6bf6ece5a0b9e4e51cfcc66cfe8e86f10
Tangi Migot <tangi.migot@gmail.com>
2025-03-20 01:21:26 -0400

    Add derivative check for Jacobian of residual (#85)
    * Add derivative check for Jacobian of residual
    
    * Move jacobian_residual_check in a file
    
    ---------
    
    Co-authored-by: Alexis Montoison <alexis.montoison@polymtl.ca>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using BundleAdjustmentModels, DataFrames, LinearAlgebra, NLPModels, Pkg, Test
 
 include("testBundleAdjustmentModels.jl")
 include("testBundleAdjustmentAllocations.jl")
+include("jacobian_residual_check.jl")
